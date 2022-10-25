d3792caffff1f6048911e34f56a26c1dd48d7df7
adamski83 <adamsik83@wp.pl>
2022-10-25 18:03:15 +0200

    added  new feature

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -8,10 +8,11 @@ import {createPost, updatePost} from "../../actions/posts";
 
 const Form = ({currentId, setCurrentId}) => {
     const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: '',
+        title: '', message: '', tags: '', selectedFile: '',
     });
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
     const classes = useStyles();
+    const user = JSON.parse(localStorage.getItem('profile'));
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -22,39 +23,50 @@ const Form = ({currentId, setCurrentId}) => {
 
     const handelSubmit = (e) => {
         e.preventDefault();
-        if (currentId) {
-            dispatch(updatePost(currentId, postData))
-
+        if (currentId === 0) {
+            dispatch(createPost({...postData, name: user?.name}))
+            clear()
         } else {
-            dispatch(createPost(postData))
+            dispatch(updatePost(currentId, {...postData, name: user?.name}))
+            clear();
         }
-        clear();
+
+    }
+    if(!user?.name) {
+        return(
+            <Paper className={classes.paper}>
+                <Typography variant='h6' align='center'>
+                    Please Sign In to create your own memories and like other's memories.
+                </Typography>
+            </Paper>
+        )
     }
 
     const clear = e => {
         setCurrentId(null);
         setPostData({
-            creator: '', title: '', message: '', tags: '', selectedFile: '',
+            title: '', message: '', tags: '', selectedFile: '',
         })
     }
     return (<Paper className={classes.paper}>
         <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handelSubmit}>
             <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
+            {/*<TextField*/}
+            {/*    name='creator'*/}
+            {/*    variant='outlined'*/}
+            {/*    label='Creator'*/}
+            {/*    fullWidth*/}
+            {/*    value={postData.creator}*/}
+            {/*    onChange={(e) => setPostData({...postData, creator: e.target.value})}*/}
+            {/*/>*/}
             <TextField
-                name='creator'
+                name='title'
                 variant='outlined'
-                label='Creator'
+                label='Title'
                 fullWidth
-                value={postData.creator}
-                onChange={(e) => setPostData({...postData, creator: e.target.value})}
+                value={postData.title}
+                onChange={(e) => setPostData({...postData, title: e.target.value})}
             /> <TextField
-            name='title'
-            variant='outlined'
-            label='Title'
-            fullWidth
-            value={postData.title}
-            onChange={(e) => setPostData({...postData, title: e.target.value})}
-        /> <TextField
             name='message'
             variant='outlined'
             label='Message'
